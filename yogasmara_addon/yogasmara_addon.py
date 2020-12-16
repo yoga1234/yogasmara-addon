f8a176c44f80593cf7d8a1f84b49c389b1f5c556
Septiana Yoga <septianayoga30@gmail.com>
2020-12-16 21:26:38 +0700

    adding file manager addon

--- yogasmara_addon/yogasmara_addon.py
+++ yogasmara_addon/yogasmara_addon.py
@@ -1,17 +1,27 @@
 import bpy
-
-class YogasmaraAddon(bpy.types.Panel):
-  bl_idname= = "PT_yogasmara_addon" # Panel Type Yogasmara Addon
-  bl_category = "Yogasmara Addon"
-  bl_space_type = "VIEW_3D" # Where the panel will placed
-  bl_region_type = "UI"
-
-
+        
+class YogasmaraFileManager(bpy.types.Panel):
+    bl_idname = "PT_yogasmara_file_manager" # Panel Type yogasmara file manager
+    bl_category = "Yogasmara Addon" # Name of the addon
+    bl_space_type = "VIEW_3D" # where the addon will be placed
+    bl_region_type = "UI"
+    bl_label = "File Manager"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.operator("wm.open_mainfile")
+        
+        row = layout.row()
+        row.operator("wm.save_mainfile")
+ 
 def register():
-  bpy.utils.register_class(YogasmaraAddon)
+    bpy.utils.register_class(YogasmaraFileManager)
 
 def unregister():
-  bpy.utils.unregister_class(YogasmaraAddon)
-
+    bpy.utils.unregister_class(YogasmaraFileManager)
+    
 if __name__ == "__main__":
-  register()
+    register()
+    
