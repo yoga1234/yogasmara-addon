b24123b86f424fcc1fc37ca6c490b911fb4f68f5
Septiana Yoga <septianayoga30@gmail.com>
2020-12-17 13:56:08 +0700

    updating addon

--- yogasmara_addon/yogasmara_addon.py
+++ yogasmara_addon/yogasmara_addon.py
@@ -1,7 +1,7 @@
 import bpy
         
 class YogasmaraFileManager(bpy.types.Panel):
-    bl_idname = "PT_yogasmara_file_manager" # Panel Type yogasmara file manager
+    bl_idname = "PANEL_PT_yogasmara_file_manager" # Panel Type yogasmara file manager
     bl_category = "Yogasmara Addon" # Name of the addon
     bl_space_type = "VIEW_3D" # where the addon will be placed
     bl_region_type = "UI"
@@ -11,17 +11,34 @@ class YogasmaraFileManager(bpy.types.Panel):
         layout = self.layout
         
         row = layout.row()
-        row.operator("wm.open_mainfile")
+        row.operator("wm.open_mainfile", text="Open File")
         
         row = layout.row()
-        row.operator("wm.save_mainfile")
+        row.operator("wm.save_mainfile", text="Save File")
+        
+class YogasmaraDeleteManager(bpy.types.Panel):
+    bl_idname = "PANEL_PT_yogasmara_delete_manager"
+    bl_category = "Yogasmara Addon"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_label = "Delete Manager"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.operator("object.delete", text="Smooth")
+        
+        
  
 def register():
     bpy.utils.register_class(YogasmaraFileManager)
+    bpy.utils.register_class(YogasmaraDeleteManager)
 
 def unregister():
     bpy.utils.unregister_class(YogasmaraFileManager)
-    
+    bpy.utils.unregister_class(YogasmaraDeleteManager)
+        
 if __name__ == "__main__":
     register()
     
