9e6c156ca6dfb78a86d7857f790cf5513d4eaeb7
Septiana Yoga <septianayoga30@gmail.com>
2020-12-17 14:11:24 +0700

    adding mesh manager

--- yogasmara_addon/yogasmara_addon.py
+++ yogasmara_addon/yogasmara_addon.py
@@ -16,28 +16,67 @@ class YogasmaraFileManager(bpy.types.Panel):
         row = layout.row()
         row.operator("wm.save_mainfile", text="Save File")
         
-class YogasmaraDeleteManager(bpy.types.Panel):
-    bl_idname = "PANEL_PT_yogasmara_delete_manager"
+class YogasmaraObjectManager(bpy.types.Panel):
+    bl_idname = "PANEL_PT_yogasmara_object_manager"
     bl_category = "Yogasmara Addon"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_label = "Delete Manager"
+    bl_label = "Object Manager"
     
     def draw(self, context):
         layout = self.layout
         
         row = layout.row()
-        row.operator("object.delete", text="Smooth")
+        row.operator("object.delete", text="Delete Selected Object")
+        
+        row = layout.row()
+        row.operator("object.select_all", text="Select All Object")
+        
+class YogasmaraMeshManager(bpy.types.Panel):
+    bl_idname = "PANEL_PT_yogasmara_mesh_manager"
+    bl_category = "Yogasmara Addon"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_label = "Mesh Manager"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.operator("mesh.primitive_plane_add", text="Add New Plane")
+        
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", text="Add New Cube")
+        
+        row = layout.row()
+        row.operator("mesh.primitive_circle_add", text="Add New Circle")
+        
+        row = layout.row()
+        row.operator("mesh.primitive_uv_sphere_add", text="Add New UV Sphere")
+        
+        row = layout.row()
+        row.operator("mesh.primitive_ico_sphere_add", text="Add New ICO Sphere")
+        
+        row = layout.row()
+        row.operator("mesh.primitive_cylinder_add", text="Add New Cylinder")
+        
+        row = layout.row()
+        row.operator("mesh.primitive_cone_add", text="Add New Cone")
+        
+        row = layout.row()
+        row.operator("mesh.primitive_torus_add", text="Add New Torus")
         
         
  
 def register():
     bpy.utils.register_class(YogasmaraFileManager)
-    bpy.utils.register_class(YogasmaraDeleteManager)
+    bpy.utils.register_class(YogasmaraObjectManager)
+    bpy.utils.register_class(YogasmaraMeshManager)
 
 def unregister():
     bpy.utils.unregister_class(YogasmaraFileManager)
-    bpy.utils.unregister_class(YogasmaraDeleteManager)
+    bpy.utils.unregister_class(YogasmaraObjectManager)
+    bpy.utils.unregister_class(YogasmaraMeshManager)
         
 if __name__ == "__main__":
     register()
